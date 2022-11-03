96158e5e974c07e7add69e4cc7fab0b5738b03b3
student <student@it.local>
2022-11-03 13:00:00 +0100

    liniowa

--- projekt.cpp
+++ projekt.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 
+using namespace std;
 
 int main()
 
 {
+int a,b,wynik;
+cin>>a>>b;
+
+if (a==0)
+{
+	wynik=(-b);
+}
+else
+{
+	wynik=(-b)/a;
+}
 
 
 
+cout<<wynik;
 
-
-
+return 0;
 }
